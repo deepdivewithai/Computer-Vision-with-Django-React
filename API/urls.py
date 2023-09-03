e7abe662ccf93b4efcee3fcbaaf8a4dbb0b83af4
Chetan <chetan.aiofficial@gmail.com>
2023-09-03 15:51:11 +0530

    first commit

---
+++ API/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'ImageAPI'
+
+urlpatterns = [
+    path('', views.ImageListView.as_view(), name='image_list'),
+    path('<int:pk>/', views.ImageDetailView.as_view(), name='image_detail'),
+]
+
+
---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from API import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='ImageAPI/index.html')),
+    path('api/', include('API.urls', namespace='ImageAPI')),
+    path('prediction/', views.prediction, name='prediction')
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
